9657e137ef8f2f585c5970e0b0cc9ca1cfe7f30b
MoriyaShiine <45156123+MoriyaShiine@users.noreply.github.com>
2024-04-05 16:04:48 -0500

    sauce no longer renders harmful effects

--- src/main/java/moriyashiine/potionsauce/client/event/RenderSauceEvent.java
+++ src/main/java/moriyashiine/potionsauce/client/event/RenderSauceEvent.java
@@ -7,6 +7,8 @@ package moriyashiine.potionsauce.client.event;
 import moriyashiine.potionsauce.common.PotionSauce;
 import net.fabricmc.fabric.api.client.item.v1.ItemTooltipCallback;
 import net.minecraft.client.item.TooltipContext;
+import net.minecraft.entity.effect.StatusEffectCategory;
+import net.minecraft.entity.effect.StatusEffectInstance;
 import net.minecraft.item.ItemStack;
 import net.minecraft.nbt.NbtCompound;
 import net.minecraft.potion.PotionUtil;
@@ -21,8 +23,12 @@ public class RenderSauceEvent implements ItemTooltipCallback {
 		if (stack.isFood()) {
 			NbtCompound nbt = stack.getSubNbt(PotionSauce.MOD_ID);
 			if (nbt != null && nbt.getBoolean("Sauced")) {
-				lines.add(1, Text.translatable("tooltip.potionsauce.sauce").formatted(Formatting.DARK_GREEN));
-				PotionUtil.buildTooltip(PotionSauce.getEffects(stack), lines, 1F);
+				List<StatusEffectInstance> effects = PotionSauce.getEffects(stack);
+				effects.removeIf(instance -> instance.getEffectType().getCategory() == StatusEffectCategory.HARMFUL);
+				if (!effects.isEmpty()) {
+					lines.add(1, Text.translatable("tooltip.potionsauce.sauce").formatted(Formatting.DARK_GREEN));
+					PotionUtil.buildTooltip(effects, lines, 1F);
+				}
 			}
 		}
 	}
